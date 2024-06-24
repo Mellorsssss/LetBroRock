17691bbd9a9520f080de42383b89a9ccdbd51e9d
Mellorsssss <1026579097@qq.com>
2024-06-24 20:27:49 +0800

    test: add some test scripts

--- src/test/evaluate_br/foo.cpp
+++ src/test/evaluate_br/foo.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 #include <unistd.h>
 
-auto add(int a, int b) -> int
+auto long_for(int loop_cnt) -> int
 {
-  return a + b;
+  int fuck_cnt = 0;
+  for (int i = 0; i < loop_cnt; i++)
+  {
+    if (i % 2)
+    {
+      fuck_cnt += 1;
+  //    printf(".");
+    }
+    else
+    {
+      fuck_cnt += 2;
+   //   printf("-");
+    }
+  }
+  return fuck_cnt;
 }
 
 void *workload(void *_)
 {
   sleep(1);
-  add(1, 3);
+  asm volatile ("" : : : "memory"); 
+  int a = long_for(10000000);
+  asm volatile ("" : : : "memory"); 
+  std::cout<<a<<std::endl;
+  exit(EXIT_SUCCESS);
   return NULL;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-  pthread_t t;
-  pthread_create(&t, NULL, workload, NULL); 
-  pthread_join(t, NULL);
+  if (atoi(argv[1]) == 1)
+  {
+    pthread_t t;
+    pthread_create(&t, NULL, workload, NULL);
+    pthread_join(t, NULL);
+  } else {
+    workload(nullptr);
+  }
+  printf("foO: we get here\n");
+  exit(EXIT_SUCCESS);
   return 0;
 }
+
